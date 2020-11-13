2322581d766ef185ba16808b4964974117e12288
hsyoon <hsyoon@wemb.co.kr>
2020-11-14 01:55:40 +0900

    Level1 Complete

--- Programmers/Hash/Level1.js
+++ Programmers/Hash/Level1.js
@@ -1,1 +1,21 @@
-console.log('test')
+// 프로그래머스 해쉬 함수 1번 문제.
+// https://programmers.co.kr/learn/courses/30/parts/12077
+
+// participant	                            completion	                    return
+// [leo, kiki, eden]	                    [eden, kiki]                     leo
+// [marina, josipa, nikola, vinko, filipa]	[josipa, filipa, marina, nikola] vinko
+// [mislav, stanko, mislav, ana]	        [stanko, ana, mislav]            mislav
+
+function solution() {
+    let participant = ['leo', 'kiki', 'eden'];
+    let completion  = ['eden', 'kiki'];
+
+    participant.sort();
+    completion.sort();
+    
+    let answer = participant.find((e, i) => e != completion[i]);
+    console.log(answer);
+    return answer;
+}
+
+solution();
