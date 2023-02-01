03a18ed88264f54f6e67b9bb6a1109cb4189fccf
soft4750 <soft4750@gmail.com>
2023-02-01 23:50:08 +0900

    feat: level0

---
+++ Programmers/level_0/pizza/index.js
@@ -0,0 +1,5 @@
+function solution(n) {
+	return n % 7 === 0 ? n / 7 : parseInt(n / 7) + 1;
+}
+
+// 머쓱이네 피자가게 -> 한판에 7조각으로 먹을 수 있는 사람 수
---
+++ Programmers/level_0/string_reverse/index.js
@@ -0,0 +1,3 @@
+function solution(string) {
+	return string.spilt("").reverse().join("");
+}
